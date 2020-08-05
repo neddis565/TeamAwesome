5c0cc36ae606d5d372670c1614c5d90c0fea1a38
JiminyC <43939609+JiminyC@users.noreply.github.com>
2020-08-05 17:24:36 +0100

    add
    Month dropdown

--- Javascript.js
+++ Javascript.js
@@ -4,3 +4,18 @@
 // function to validate 18+
 
 //functions for interactive dashboard
+
+
+//dropdown months
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+"November", "December"];
+
+var selectMonth = document.getElementById("month");
+
+for (var i = 0; i < month.length; i++) {
+    var option = month[i];
+    var newElement = document.createElement("option");
+    newElement.textContent = option;
+    newElement.value = option;
+    selectMonth.appendChild(newElement);
+}
